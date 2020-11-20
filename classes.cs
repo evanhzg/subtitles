8b4822a560cb74378917573ea56f1338a7aac51d
evan <evan.hoizey@ynov.com>
2020-11-20 15:38:19 +0100

    Ajout de la variable between et de commentaires. projet terminé

--- classes.cs
+++ classes.cs
@@ -9,23 +9,20 @@ namespace SousTitresProj
 {
     public class Srt
     {
+        // définition des variables globales utilisées dans plusieurs fonctions/Tasks.
         public DateTime defaultTime = DateTime.Now;
         public DateTime curTimecode = new DateTime();
-        public DateTime subtitleStart = new DateTime();
         public DateTime subtitleEnd = new DateTime();
-        public DateTime startSub = new DateTime();
-        public DateTime endSub = new DateTime();
+        public DateTime subtitleStart = new DateTime();
         public TimeSpan subLength = new TimeSpan();
+        public TimeSpan between = new TimeSpan();
         public TimeSpan curTime = new TimeSpan();
-        public TimeSpan nextSub = new TimeSpan();
-        public TimeSpan beforeNext = new TimeSpan();
-        public int premierSoustitre = 1;
-        public int compDatesStart = 0;
-        public int compDatesEnd = 0;
-        public int endOfSub = 0;
-        public int shown = 0;
-        public string format = "HH:mm:ss,fff";
 
+        public string subtitleEndStr = "00:00:00.000"; // première valeur déterminant l'attente avant le premier sous-titre
+        public int endOfSub = 0; // si affiché, fin du timer.
+        public string format = "HH:mm:ss,fff"; // détermine le format d'affichage du timecode dans le fichier srt.
+
+        // Tâche Stream qui affiche et écrit les sous-titres dans la console et le fichier new_subs.srt
         public async Task Stream()
         {
             // Définition du dossier Desktop et du chemin des fichiers de sous-titres
@@ -39,72 +36,67 @@ namespace SousTitresProj
             // StreamWriter permet d'écrire dans un nouveau fichier (ici newFilePath) les données souhaitées
             using (StreamWriter sw = new StreamWriter(newFilePath))
             {
-                string subtitle = "";
                 string l = "";
                 List<string> lines = new List<string>();
+                subtitleEnd = DateTime.Parse(subtitleEndStr);
                 while ((l = sr.ReadLine()) != null)
                 {
                     if (l.Contains(" --> "))
                     {
-                        //Divise les lignes de timecode en deux variables datetime distinctes
+                        // Divise les lignes de timecode en deux variables datetime distinctes
+                        // et stocke l'attente entre et pendant les sous-titres dans des TimeSpan
                         string[] subtitleSplit = l.Split(new[] { " --> " }, StringSplitOptions.None);
                         string subtitleStartStr = subtitleSplit[0];
                         subtitleStartStr = subtitleStartStr.Replace(",", ".");
 
-                        string subtitleEndStr = subtitleSplit[1];
+                        subtitleEndStr = subtitleSplit[1];
                         subtitleEndStr = subtitleEndStr.Replace(",", ".");
 
                         subtitleStart = DateTime.Parse(subtitleStartStr);
+                        between = subtitleStart - subtitleEnd;
                         subtitleEnd = DateTime.Parse(subtitleEndStr);
+
                         subLength = subtitleEnd - subtitleStart;
+
+                        sw.WriteLine("");
                         sw.WriteLine(subtitleStartStr + " - " + subtitleEndStr);
-                        if (premierSoustitre == 1)
-                        {
-                            subLength = subtitleStart - defaultTime;
-                        }
                         continue;
                     }
-                    // Tant que la ligne n'est pas vide (soit la ligne qui suit les sous-titres),
-                    // la variable subtitle la stocke et l'affiche dans la console et dans le newFile,
-                    // ainsi que les timecode de début et de fin.
+                    // Si la variable contient plus de 4 caractères (les sous-titres),
+                    // l'affiche et l'écrit dans le fichier
                     else if (l.Length >= 4)
                     {
-                        if (premierSousTitre == 1)
-                        {
-                            System.Threading.Thread.Sleep(subLength);
-                            premierSoustitre = 0;
-
-                            subLength = subtitleEnd - subtitleStart;
-                        }
+                        System.Threading.Thread.Sleep(between);
                         sw.WriteLine(l);
                         Console.WriteLine(l);
                         System.Threading.Thread.Sleep(subLength);
                         Console.Clear();
-                    }
-                    else
-                    {
                         continue;
                     }
+                    else
+                        continue;
                 }
+                // détermine la fin du timer (inutilisé  mais fonctionnel)
                 endOfSub = 1;
             }
         }
 
+        // Fonction/Task déterminant le temps actuel, n'est pas utilisée dans le programme
+        // mais détermine un timer en continu qui peut déterminer l'avancée.
         public async Task Timer()
         {
-            curTime = curTimecode - defaultTime;
-            string defaultTimeStr = defaultTime.ToString("HH:mm:ss.fff");
-            string curTimeStr = curTimecode.ToString("HH:mm:ss.fff");
+            string defaultTimeStr = defaultTime.ToString(format);
+            string curTimeStr = curTimecode.ToString(format);
             while (endOfSub != 1)
             {
                 await Task.Delay(1);
                 curTimecode = DateTime.Now;
-                curTimeStr = curTimecode.ToString("HH:mm:ss.fff");
-                compDatesStart = curTime.CompareTo(subtitleStart);
-                compDatesEnd = curTime.CompareTo(subtitleEnd);
+                curTimeStr = curTimecode.ToString(format);
             }
         }
     }
+
+    // fonction pause mise de côté car non fonctionnelle et j'ai préféré me concentrer sur le reste.
 
     //public class Pause
     //{
